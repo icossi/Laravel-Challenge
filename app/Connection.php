277aab56f5a3249c273151cd5c7bd060022a6a78
Ivan Cossi Camacho <cossi.uaa@gmail.com>
2018-05-03 13:12:04 -0500

    Changes Last Version

--- app/Connection.php
+++ app/Connection.php
@@ -10,18 +10,29 @@ class Connection extends Model
       'status',
       'organization'
     ];
+    /**
+     * This function connect with the API and return an array with the repositories
+     */
     public function connect($url){
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       curl_setopt($ch,CURLOPT_URL,"$url");
       curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
       curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
-      curl_setopt($ch,CURLOPT_HTTPAUTH,CURLAUTH_BASIC);
-      curl_setopt($ch, CURLOPT_USERPWD, "icossi:dancewithme@@12");
+      $usergithub=env('GIT_USER', 'Laravel');
+      $passgithub=env('GIT_PASS', 'Laravel');
+      if($usergithub!='Laravel' && $passgithub!='Laravel'){ 
+        curl_setopt($ch,CURLOPT_HTTPAUTH,CURLAUTH_BASIC); // basic autentication
+        curl_setopt($ch, CURLOPT_USERPWD, "$usergithub:$passgithub"); // user and password for autentication with github
+      }
       $data = json_decode(curl_exec($ch));
       curl_close($ch);
       return $data;
     }
+
+    /**
+     * return the last connection to the appi
+     */
     public static function getLastConecction(){
       return Connection::all()->last();
     }
--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -9,6 +9,7 @@ use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 use Carbon\Carbon;
 
+
 class RepositoryController extends Controller
 {
   protected $orgName="githubtraining";
@@ -36,38 +37,42 @@ class RepositoryController extends Controller
       $conn->organization=$orgName;
       $conn->status=0;
       $conn->save();
-      $url="https://api.github.com/orgs/$orgName/repos";
+      $url="https://api.github.com/orgs/$orgName/repos"; // the organization url
       $data=$conn->connect($url);
       $repositories=collect([]);
       $count=0;
       $last=count($data)-10;
-      foreach($data as $repo){
-        $name=$repo->name;
-        $comitsUrl="https://api.github.com/repos/$orgName/$name/commits";
-        $comits=$conn->connect($comitsUrl);
-        $repository=new Repository();
-        $repository->name=$name;
-        $ct=Carbon::parse($repo->created_at);
-        $repository->creation_date=$ct->toDateTimeString();
-        if(isset($comits[0]->commit)){
-          $ls=Carbon::parse($comits[0]->commit->committer->date);
-          $repository->last_commit=$ls->toDateTimeString();
-        }else{
-          $repository->last_commit=null;
-        }
+      if(is_array($data)){
+          foreach($data as $repo){
+            $name=$repo->name;
+            $comitsUrl="https://api.github.com/repos/$orgName/$name/commits";
+            $comits=$conn->connect($comitsUrl);         // Get the commits
+            $repository=new Repository();
+            $repository->name=$name;
+            $ct=Carbon::parse($repo->created_at);
+            $repository->creation_date=$ct->toDateTimeString();
+            if(is_array($comits)){
+              if(isset($comits[0]->commit)){
+                $ls=Carbon::parse($comits[0]->commit->committer->date);
+                $repository->last_commit=$ls->toDateTimeString();
+              }else{
+                $repository->last_commit=null;
+              }
 
-        $repository->organization=$orgName;
-        $repository->description=$repo->description;
-        if($count>$last){
-          $repository->save();
+              $repository->organization=$orgName;
+              $repository->description=$repo->description;
+              if($count>$last){
+                $repository->save();
+              }
+              $count+=1;
+              $repositories->push($repository);
+            }
+          }
+          if($count>0){ // if the connection return any repository change status to 1
+            $conn->status=1;
+            $conn->save();
+          }
         }
-        $count+=1;
-        $repositories->push($repository);
+        return $repositories;
       }
-      if($count>0){
-        $conn->status=1;
-        $conn->save();
-      }
-      return $repositories;
-    }
 }
